aafea5e16dd00ecf1cea81c39e4e2741459de43c
BrazhnikDA <dimity.brazhnik@yandex.ru>
2021-05-23 14:56:34 +0300

    Добавьте файлы проекта.

---
+++ OS_LAB_2_Client/Client.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OS_LAB_2_Client
+{ 
+    class Client
+    {
+        private const string host = "127.0.0.1";    // IP
+        private const int port = 7770;              // Порт
+        TcpClient client;                           // Клиент
+        NetworkStream stream;                       // Поток от клиента до сервера
+        
+        public bool IsConnected { get; set; }
+
+        public Client()
+        {
+
+        }
+
+        public void serverConnect()
+        {
+            // Создаём клиент
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port);     // Подключение клиента
+                stream = client.GetStream();    // Получаем поток
+                IsConnected = true;
+                // Запускаем новый поток для получения данных от сервера
+                Thread receiveThread = new Thread(new ThreadStart(receiveMessage));
+                receiveThread.Start(); //старт потока
+            }
+            catch (Exception ex)
+            {
+                // Вывод ошибки в консоль, скорее всего сервер не запущен, или случилась хрень
+                Console.WriteLine(ex.Message);
+                //MessageBox.Show("Сервер не отвечает", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public bool checkConnection()
+        {
+            if (client == null) return false;
+
+            if (!client.Connected)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void receiveMessage()
+        {
+            // Бесконечно слушаем
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[128]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable); 
+                    Console.WriteLine("Error: " + builder.ToString());      // Вывод полученного сообщения
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!"); // Соединение было прервано
+                    //MessageBox.Show("Подключение разрвано, приложение закроется через 5 секунд", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    //Thread.Sleep(5000);
+                    disconnect();
+                }
+            }
+        }
+
+        private void disconnect()
+        {
+            if (stream != null)
+                stream.Close();     // Отключение потока
+            if (client != null)
+                client.Close();     // Отключение клиента
+            Environment.Exit(0);    // Закртиые приложения
+        }
+
+        public void sendMessage(string message)
+        {
+            if ((message != ""))
+            {
+                byte[] buffer = new byte[1024];
+                buffer = Encoding.UTF8.GetBytes(message);   // Делаем байт код в формате UTF-8(Это важно) и отправляем его на сервер
+                stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
---
+++ OS_LAB_2_Client/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+namespace OS_LAB_2_Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int currentRowIndex;
+        bool IsClick = false;
+        Client client;
+        private BindingList<Tech> toData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            client = new Client();
+            toData = new BindingList<Tech>();
+        }
+
+        private void buttonSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (toData.Count != 0)
+            {
+                if (client.IsConnected == false)
+                {
+                    client.serverConnect();
+                }
+                if (client.checkConnection())
+                {
+                    for (int i = 0; i < toData.Count; i++)
+                    {
+                        client.sendMessage(buildJSON(i));
+                        Thread.Sleep(20);
+                    }
+                }
+                else MessageBox.Show("Соедение с сервером не установленно!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else { MessageBox.Show("Таблица пуста!", "Предупреждение!", MessageBoxButton.OK, MessageBoxImage.Warning); }
+        }
+        private string buildJSON(int index)
+        {
+            var technik = new Tech();
+            {
+                technik.name = toData[index].name;
+                technik.marka = toData[index].marka;
+                technik.tech = toData[index].tech;
+                technik.serNum = toData[index].serNum;
+                technik.faultClient = toData[index].faultClient;
+                technik.date = toData[index].date;
+            }
+            string jsonData = JsonConvert.SerializeObject(technik);
+            Console.WriteLine(index + ". " + jsonData);
+            return jsonData;
+        }
+
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            currentRowIndex = tableView.Items.IndexOf(tableView.CurrentItem);
+
+            textBoxFIO.Text = toData[currentRowIndex].name;
+            textBoxMarks.Text = toData[currentRowIndex].marka;
+            textBoxFault.Text = toData[currentRowIndex].faultClient;
+            textBoxSerNum.Text = toData[currentRowIndex].serNum;
+            comboBoxTechnic.Text = toData[currentRowIndex].tech;
+            pickerDate.Text = toData[currentRowIndex].date;
+
+            IsClick = true;
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (isEmptyData())
+            {
+                if (IsClick)
+                {
+                    toData[currentRowIndex].name = textBoxFIO.Text;
+                    toData[currentRowIndex].marka = textBoxMarks.Text;
+                    toData[currentRowIndex].faultClient = textBoxFault.Text;
+                    toData[currentRowIndex].serNum = textBoxSerNum.Text;
+                    toData[currentRowIndex].tech = comboBoxTechnic.Text;
+                    toData[currentRowIndex].date = pickerDate.Text;
+
+                    tableView.ItemsSource = null;
+                    tableView.ItemsSource = toData;
+
+                    IsClick = false;
+                    clearData();
+                }
+                else
+                {
+                    newRecordTable();
+                    clearData();
+                    tableView.ItemsSource = toData;
+                }
+            }
+            else { MessageBox.Show("Не все поля были заполненны", "Предупреждение!", MessageBoxButton.OK, MessageBoxImage.Warning); }
+
+        }
+
+        private void buttonRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsClick)
+            {
+                toData.RemoveAt(currentRowIndex);
+                tableView.ItemsSource = null;
+                tableView.ItemsSource = toData;
+
+                IsClick = false;
+                clearData();
+            }
+            else { MessageBox.Show("Ячейка не выбрана", "Предупреждение!", MessageBoxButton.OK, MessageBoxImage.Warning); }
+        }
+
+            private void newRecordTable()
+        {
+            toData.Add(new Tech()
+            {
+                name = textBoxFIO.Text,
+                marka = textBoxMarks.Text,
+                tech = comboBoxTechnic.Text,
+                serNum = textBoxSerNum.Text,
+                faultClient = textBoxFault.Text,
+                date = pickerDate.Text
+            });
+        }
+
+        private void clearData()
+        {
+            textBoxFIO.Text = "";
+            textBoxMarks.Text = "";
+            textBoxFault.Text = "";
+            textBoxSerNum.Text = "";
+            comboBoxTechnic.Text = "";
+            pickerDate.Text = "";
+        }
+
+        private bool isEmptyData()
+        {
+            if(textBoxFIO.Text == "" && textBoxMarks.Text == "" && comboBoxTechnic.Text == "" &&
+                textBoxSerNum.Text == "" && textBoxFault.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                if (pickerDate.Text == "")
+                {
+                    pickerDate.SelectedDate = DateTime.Now;
+                }
+                return true;
+            }
+        }
+    }
+}
